a6f9cbf25e512b1e4a655976ff769a6f26235ad9
Omar Issa <omom2168@gmail.com>
2025-10-16 22:01:28 +0300

    Fix numericFilters and improve the api logic

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,10 @@
-import product from "../models/product.js";
+import Product from "../models/product.js";
 
-const getAllProductsStatic = async (req, res) => {
-  const products = await product.find({ price: { $gt: 30 } }).sort("price");
-  res.status(200).json({ products, nbHits: products.length });
-};
 const getAllProducts = async (req, res) => {
   const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
   if (featured) {
-    queryObject.featured = featured === "true" ? true : false;
+    queryObject.featured = featured === "true";
   }
   if (company) {
     queryObject.company = company;
@@ -16,6 +12,8 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
+
+  // console.log(queryObject);
 
   // Numeric Filters
   if (numericFilters) {
@@ -33,19 +31,21 @@ const getAllProducts = async (req, res) => {
       regEx,
       (match) => `-${operatorMap[match]}-`
     );
-
-    const options = ["price", "rating"];
+    // "discount", "stock" for later use
+    const options = ["price", "rating", "discount", "stock"];
 
     filters.split(",").forEach((item) => {
       const [field, operator, value] = item.split("-");
-      if (options.includes(field)) {
+      const num = Number(value);
+      if (!isNaN(num) && options.includes(field)) {
         if (!queryObject[field]) queryObject[field] = {};
-        queryObject[field][operator] = Number(value);
+        queryObject[field][operator] = num;
       }
     });
+    // console.log("numericFilters:", req.query.numericFilters);
   }
-  console.log(queryObject);
-  let result = product.find(queryObject);
+  let result = Product.find(queryObject);
+
   if (sort) {
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
@@ -63,7 +63,12 @@ const getAllProducts = async (req, res) => {
   result = result.skip(skip).limit(limit);
   // console.log(page, limit, skip);
 
-  const products = await result;
+  const products = await result.lean();
+
+  if (products.length === 0) {
+    return res.status(200).json({ msg: "No products found", nbHits: 0 });
+  }
   res.status(200).json({ products, nbHits: products.length });
 };
-export { getAllProducts, getAllProductsStatic };
+
+export { getAllProducts };
--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,5 @@
 import express from "express";
-import {
-  getAllProducts,
-  getAllProductsStatic,
-} from "../controllers/products.js";
+import { getAllProducts } from "../controllers/products.js";
 const router = express.Router();
 router.route("/").get(getAllProducts);
-router.route("/static").get(getAllProductsStatic);
 export default router;
