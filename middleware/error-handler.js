70fb604a69e6a9ffa2264d7641a31116ae0e721c
Omar Issa <omom2168@gmail.com>
2025-10-16 21:05:06 +0300

    add the Numeric Filters and end all sorting and end the store api MVP

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,5 @@
 const errorHandlerMiddleware = async (err, req, res, next) => {
-  console.log(`Error caught by middleware: ${err.message}`);
+  console.log(`Error caught by middleware: ${err}`);
   return res
     .status(500)
     .json({ msg: "Something went wrong, please try again" });
